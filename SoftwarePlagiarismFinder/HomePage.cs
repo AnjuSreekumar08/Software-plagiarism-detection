d48ad5336a63c04d1c77dd7d6b1931b663bfdbb5
AnjuSreekumar08 <69891316+AnjuSreekumar08@users.noreply.github.com>
2021-04-19 21:34:05 +0530

    Add files via upload

---
+++ SoftwarePlagiarismFinder/HomePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoftwarePlagiarismFinder
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            OTO_BrowseFiles obj = new OTO_BrowseFiles();
+            obj.Show();
+        }
+
+        private void toolStripLabel9_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
